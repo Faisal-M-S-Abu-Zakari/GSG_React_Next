7ddf2d4c666fd8898fcb49e5bfa0284f30ca958a
Faisal <zkryfysl@gmail.com>
2025-03-08 09:28:27 +0200

    commit

--- 4_news_app/services/news.service.ts
+++ 4_news_app/services/news.service.ts
@@ -1,24 +1,23 @@
-import { notFound } from "next/navigation";
+import { News } from "@/types";
+// import { notFound } from "next/navigation";
 
-const api_key = 'pub_701076cdd4cdeaa56df41b17fae04f1ce8350';
+const api_key = "pub_701076cdd4cdeaa56df41b17fae04f1ce8350";
 
 const fetchNews = async (category: string, country: string) => {
   const res = await fetch(
     `https://newsdata.io/api/1/latest?apikey=${api_key}&category=${category}&country=${country}`,
-    { method: 'GET', cache: 'no-store' }
+    { method: "GET", cache: "no-store" }
   );
 
   const newsRes = (await res.json()) as News.IResponse;
   let latestNews: News.Item[] = [];
-  if (newsRes.status === 'success') {
-    latestNews = newsRes.results.map(item => (
-      {
-        id: item.article_id,
-        title: item.title,
-        img: item.image_url,
-        content: item.description
-      }
-    ));
+  if (newsRes.status === "success") {
+    latestNews = newsRes.results.map((item) => ({
+      id: item.article_id,
+      title: item.title,
+      img: item.image_url,
+      content: item.description,
+    }));
   } else {
     // triggering notFound manually
     // notFound();
@@ -26,12 +25,12 @@ const fetchNews = async (category: string, country: string) => {
 
   // throw new Error("Something went wrong while fetching the News from server [xa2658]");
 
-  // The goal of the promise below is to make the response slower (just to demo loading status) 
-  return new Promise((resolve) => setTimeout(() => {
-    resolve(latestNews);
-  }, 1000));
-}
+  // The goal of the promise below is to make the response slower (just to demo loading status)
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(latestNews);
+    }, 1000)
+  );
+};
 
-export {
-  fetchNews
-}
+export { fetchNews };
